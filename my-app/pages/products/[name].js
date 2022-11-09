f26f84132f2d32b2fb9eb2c4d905051ffa466458
YuiFutao <yui.futao@gmail.com>
2022-11-09 19:05:14 +0900

    リンクの設定

--- my-app/pages/products/[name].js
+++ my-app/pages/products/[name].js
@@ -1,3 +1,6 @@
-export default function Name() {
-  return <h1>商品のページです</h1>;
+import { useRouter } from "next/router";
+export default function Color() {
+  const router = useRouter();
+  console.log(router.query)
+  return <h1>{router.query.name}の{router.query.color}カラーです</h1>;
 }
