2e9f34f86b9e5279aa5c7fbe5008a98405237b8c
YuiFutao <yui.futao@gmail.com>
2022-11-09 19:26:20 +0900

    https://reffect.co.jp/react/next-js#SEO まで完了

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
+import Head from 'next/head';
+import Image from 'next/image';
+
+
 export default function Home() {
   return (
     <div>
-      <ul>
-        <li>
-          <Link href="/about">
-            About
-          </Link>
-        </li>
-      </ul>
+      <Head>
+        <title>トップページ</title>
+        <meta property="og:title" content="タイトル" />
+        <meta property="og:type" content="website　または　blog　または　article" />
+        <meta property="og:description" content="ページの簡単な説明" />
+        <meta property="og:url" content="ページのURL" />
+        <meta property="og:site_name" content="サイトのタイトル" />
+        <meta property="og:image" content="サムネイル画像のURL" />
+      </Head>
       <h1>Hello Next.js</h1>
+      <Image src="/00005 - icon.png" width={300} height={300} />
+
     </div>
   );
 }
